2da69b1d76d9e14abdb1a3fefda094ce35f2d471
Almas Khan <almas.rbm@gmail.com>
2022-04-18 11:57:34 +0600

    extra login (Github) added

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
@@ -8,23 +8,29 @@ import Loading from '../../Shared/Loading/Loading';
 const SocialLogin = () => {
     //google sign in
     const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    //Github sign in
+    const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+    //--------------
     const navigate = useNavigate();
     const location = useLocation();
     const from = location.state?.from?.pathname || '/';
     let errorElement;
-    if (error) {
+    if (error || error1) {
         errorElement = <div>
-            <p className='text-danger'>Error: {error?.message}</p>
+            <p className='text-danger'>Error: {error?.message} {error1?.message}</p>
         </div>
     }
-    if (loading) {
+    if (loading || loading1) {
         return <Loading />;
     }
-    if (user) {
+    if (user || user1) {
         navigate(from, { replace: true });
     }
     const handleGoogleSignIn = () => {
         signInWithGoogle();
+    }
+    const handleGithubSignIn = () => {
+        signInWithGithub();
     }
     return (
         <div>
@@ -36,7 +42,7 @@ const SocialLogin = () => {
             {errorElement}
             <div>
                 <Button onClick={handleGoogleSignIn} className='w-50 d-block mx-auto my-2'>Sign with Google</Button>
-                <Button className='w-50 d-block mx-auto'>Sign with Github</Button>
+                <Button onClick={handleGithubSignIn} className='w-50 d-block mx-auto'>Sign with Github</Button>
             </div>
         </div>
     );
